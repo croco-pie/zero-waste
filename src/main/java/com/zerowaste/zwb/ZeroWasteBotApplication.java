3851dedfe117e87d438e929b889043a4ed0d6926
crocopie <nikulina.annaa@yandex.ru>
2021-04-07 19:44:22 +0300

    Initial commit

---
+++ src/main/java/com/zerowaste/zwb/ZeroWasteBotApplication.java
@@ -0,0 +1,13 @@
+package com.zerowaste.zwb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroWasteBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroWasteBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerowaste/zwb/ZeroWasteBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerowaste.zwb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroWasteBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
